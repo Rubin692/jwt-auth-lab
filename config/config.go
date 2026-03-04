afe74dd6ee05b967e122ebdda8b6e95a15984752
Rubin692 <attek.rubin.larin@gmail.com>
2026-03-04 15:07:27 +0300

    Create config.go

---
+++ config/config.go
@@ -0,0 +1,64 @@
+package config
+
+import (
+    "encoding/json"
+    "fmt"
+    "os"
+    "jwt-auth-service/models"
+)
+
+type Config struct {
+    ServerPort string          `json:"server_port"`
+    TargetAPI  string          `json:"target_api"`
+    Clients    []models.Client `json:"clients"`
+}
+
+var AppConfig Config
+
+func LoadConfig() error {
+    defaultConfig := Config{
+        ServerPort: "8080",
+        TargetAPI:  "https://jsonplaceholder.typicode.com",
+        Clients: []models.Client{
+            {
+                ID:            "client1",
+                Name:          "Test Client 1",
+                AllowedPaths:  []string{"/posts", "/comments"},
+                AllowedMethods: []string{"GET", "POST"},
+                TokenTTL:      15,
+                SecretKey:     "secret-key-1",
+            },
+            {
+                ID:            "client2",
+                Name:          "Test Client 2",
+                AllowedPaths:  []string{"/users", "/albums"},
+                AllowedMethods: []string{"GET"},
+                TokenTTL:      30,
+                SecretKey:     "secret-key-2",
+            },
+        },
+    }
+
+    file, err := os.ReadFile("config.json")
+    if err != nil {
+        fmt.Println("Config file not found, using default configuration")
+        AppConfig = defaultConfig
+        return nil
+    }
+
+    err = json.Unmarshal(file, &AppConfig)
+    if err != nil {
+        return fmt.Errorf("error parsing config file: %v", err)
+    }
+
+    return nil
+}
+
+func GetClientByID(clientID string) *models.Client {
+    for _, client := range AppConfig.Clients {
+        if client.ID == clientID {
+            return &client
+        }
+    }
+    return nil
+}
